ad22ecb5b63331a2e89ee3bdf65e281dd9e333dd
haimin <med53@mail.ru>
2020-05-17 12:51:49 +0300

    first test added

--- courses/tests.py
+++ courses/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
-from .models import Subject
+from .models import Subject, Course
+from django.contrib.auth.models import User
+
 # Create your tests here.
+
 
 class SubjectModelTest(TestCase):
     @classmethod
@@ -18,3 +21,34 @@ class SubjectModelTest(TestCase):
 
         expected_object_name = f'{subject.slug}'
         self.assertEquals(expected_object_name, 'chem')
+
+
+class CourseTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+
+        # Create a user
+        testuser1 = User.objects.create_user(username='testuser1', password='abc123')
+        testuser1.save()
+        test_subject = Subject.objects.create(title='Chemistry', slug='chem')
+        # Create a Course
+        test_course = Course.objects.create(owner=testuser1,
+                                            subject=test_subject,
+                                            title='C2H5OH',
+                                            slug='c2h5oh',
+                                            overview='overview')
+        test_course.save()
+
+    def test_course_content(self):
+        course = Course.objects.get(id=1)
+        owner = f'{course.owner}'
+        subject = f'{course.subject}'
+        title = f'{course.title}'
+        slug = f'{course.slug}'
+        overview = f'{course.overview}'
+
+        self.assertEqual(owner, 'testuser1')
+        self.assertEqual(subject, 'Chemistry')
+        self.assertEqual(title, 'C2H5OH')
+        self.assertEqual(slug, 'c2h5oh')
+        self.assertEqual(overview, 'overview')
