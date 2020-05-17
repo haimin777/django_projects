3fb1fe40d1e589cfb824f2dcf2d74d4d83851c20
haimin <med53@mail.ru>
2020-05-17 12:24:09 +0300

    first test added

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from .models import Subject
 # Create your tests here.
+
+class SubjectModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Subject.objects.create(title='Chemistry', slug='chem')
+
+    def test_title_content(self):
+        subject = Subject.objects.get(id=1)
+
+        expected_object_name = f'{subject.title}'
+        self.assertEquals(expected_object_name, 'Chemistry')
+
+    def test_slug_content(self):
+        subject = Subject.objects.get(id=1)
+
+        expected_object_name = f'{subject.slug}'
+        self.assertEquals(expected_object_name, 'chem')
